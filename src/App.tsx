622b597891c3e1ed0d3d2bfddd0062cd23697c1e
jithyan <jithyan>
2022-12-01 22:54:53 +1100

    Add basic tailwind support

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,7 @@ export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen
-                    name="Home"
-                    component={Home}
-                    options={{ title: "Welcome" }}
-                />
+                <Stack.Screen name="Home" component={Home} />
                 <Stack.Screen name="Article" component={Article} />
             </Stack.Navigator>
             <StatusBar style="auto" />
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -18,7 +18,7 @@ function LatestStories() {
     const [latestStories] = useAtom(latestArticlesList);
 
     return (
-        <>
+        <View className="flex-1 items-center justify-center bg-slate-800">
             {latestStories.map(({ id, title, imageURL }) => (
                 <View onTouchStart={() => {}} key={id}>
                     <Image
@@ -28,22 +28,13 @@ function LatestStories() {
                     <Text>{title}</Text>
                 </View>
             ))}
-        </>
+        </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
-
 export function Home() {
     return (
-        <View style={styles.container}>
+        <View className="flex-1 items-center justify-center bg-slate">
             <Suspense fallback={<LoadingSpinner />}>
                 <LatestStories />
             </Suspense>
