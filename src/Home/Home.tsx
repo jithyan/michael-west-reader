e5e02ef79bb7df450a065c0ed64e5102904da18f
jithyan <jithyan>
2022-12-03 00:06:20 +1100

    Use FlatList instead of ScrollView

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,10 +1,10 @@
 import React, { Suspense } from "react";
-import { SafeAreaView, ScrollView, Text, View, Image } from "react-native";
+import { SafeAreaView, FlatList, Text, View, Image } from "react-native";
 import { atom, useAtom } from "jotai";
 
 import { LoadingSpinner } from "../core/components";
 import { getLatestStoriesPage } from "./api";
-import { parseLatestStoriesPage } from "./parser";
+import { ArticleDescription, parseLatestStoriesPage } from "./parser";
 
 const latestArticlesList = atom(async (get) => {
     const [success, pageBody] = await getLatestStoriesPage();
@@ -14,34 +14,41 @@ const latestArticlesList = atom(async (get) => {
     return Promise.reject("Failed to fetch latest stories");
 });
 
+function Story({ id, title, imageURL, author, published }: ArticleDescription) {
+    return (
+        <View
+            className="flex-initial flex-row p-1"
+            onTouchStart={() => {}}
+            key={id}
+        >
+            <View className="basis-2/3">
+                <Text className="text-zinc-300 font-extrabold text-xl p-0.5">
+                    {title}
+                </Text>
+                <View className="flex-initial flex-row">
+                    <Text className="text-zinc-300 font-light text-xs mr-1">
+                        {author}
+                    </Text>
+                    <Text className="text-zinc-300 font-light text-xs">
+                        {published}
+                    </Text>
+                </View>
+            </View>
+            <Image className="basis-1/3" source={{ uri: imageURL }} />
+        </View>
+    );
+}
+
 function LatestStories() {
     const [latestStories] = useAtom(latestArticlesList);
 
     return (
-        <ScrollView className="bg-stone-800 ">
-            {latestStories.map(({ id, title, imageURL, author, published }) => (
-                <View
-                    className="flex-initial flex-row "
-                    onTouchStart={() => {}}
-                    key={id}
-                >
-                    <View className="basis-2/3 p-2">
-                        <Text className="text-zinc-300 font-extrabold text-xl">
-                            {title}
-                        </Text>
-                        <View className="flex-initial flex-row">
-                            <Text className="text-zinc-300 font-light text-xs">
-                                {author.trim()}
-                            </Text>
-                            <Text className="text-zinc-300 font-light text-xs">
-                                {published.trim()}
-                            </Text>
-                        </View>
-                    </View>
-                    <Image className="basis-1/3" source={{ uri: imageURL }} />
-                </View>
-            ))}
-        </ScrollView>
+        <FlatList
+            data={latestStories}
+            renderItem={({ item }) => <Story {...item} />}
+            keyExtractor={(item) => item.id}
+            className="bg-stone-800 "
+        />
     );
 }
 
