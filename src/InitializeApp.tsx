3eae2c7e7047dd1ca59bb89d19c8eda8cb18a6f7
jithyan <jithyan>
2022-12-14 17:27:46 +1100

    InitializeApp should also prune total parapgraphs

--- src/InitializeApp.tsx
+++ src/InitializeApp.tsx
@@ -1,7 +1,10 @@
 import { Suspense, useEffect } from "react";
 import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
 import { LoadingSpinner } from "~core/components";
-import { paragraphsReadAtom } from "~screens/ArticleScreen/article-state";
+import {
+    paragraphsReadAtom,
+    totalNumParagraphsAtom,
+} from "~screens/ArticleScreen/article-state";
 import { latestArticlesList } from "~screens/LatestArticlesScreen/articles-list-state";
 
 export function InitializeApp({ children }: { children: JSX.Element }) {
@@ -20,17 +23,20 @@ export function InitializeApp({ children }: { children: JSX.Element }) {
 }
 
 function LoadAtomsSyncedToStorage({ children }: { children: JSX.Element }) {
-    const v = useRecoilValue(paragraphsReadAtom);
-    return v ? children : null;
+    const paragraphsRead = useRecoilValue(paragraphsReadAtom);
+    const totalNumParagraphs = useRecoilValue(totalNumParagraphsAtom);
+    return paragraphsRead && totalNumParagraphs ? children : null;
 }
 
 function PruneItemsFromStorage() {
     const latestStories = useRecoilValue(latestArticlesList);
     const setParagraphsRead = useSetRecoilState(paragraphsReadAtom);
+    const setTotalParagraphs = useSetRecoilState(totalNumParagraphsAtom);
 
     useEffect(() => {
+        const currentStoryIds = new Set(latestStories.map((s) => s.id));
+
         setParagraphsRead((prev) => {
-            const currentStoryIds = new Set(latestStories.map((s) => s.id));
             const staleIdsToRemove = [];
 
             prev.forEach((_, k) => {
@@ -39,7 +45,26 @@ function PruneItemsFromStorage() {
                 }
             });
 
-            console.debug("staleIdsToRemove", staleIdsToRemove);
+            console.debug(
+                "staleIdsToRemove - paragraphs read",
+                staleIdsToRemove
+            );
+            return prev.deleteAll(staleIdsToRemove);
+        });
+
+        setTotalParagraphs((prev) => {
+            const staleIdsToRemove = [];
+
+            prev.forEach((_, k) => {
+                if (!currentStoryIds.has(k)) {
+                    staleIdsToRemove.push(k);
+                }
+            });
+
+            console.debug(
+                "staleIdsToRemove - total paragraphs",
+                staleIdsToRemove
+            );
             return prev.deleteAll(staleIdsToRemove);
         });
     }, [latestStories]);
